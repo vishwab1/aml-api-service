89e9d103a4c41dc997c2dd8a926c77d4fc074e87
Novfal-202 <novfal@navdhaiti.com>
2024-09-30 17:00:04 +0530

    naming convention updated

--- src/services/content.ts
+++ src/services/content.ts
@@ -3,16 +3,16 @@ import { Content } from '../models/content'; // Import Content model
 
 // Get a media Content by ID
 export const getContentMediaById = async (getObject: { contentId: number; mediaIds: string[] }): Promise<any> => {
-  const conditions: any = { id: getObject.contentId };
+  const whereClause: any = { id: getObject.contentId };
   if (getObject.mediaIds) {
     const mediaConditions = getObject.mediaIds.flatMap((mediaId) => ({
       [Op.contains]: `${mediaId}`,
     }));
-    conditions.media = mediaConditions;
+    whereClause.media = mediaConditions;
   }
 
   const mediaMetaData = await Content.findOne({
-    where: conditions,
+    where: whereClause,
     attributes: ['id', 'media'],
     raw: true,
   });
