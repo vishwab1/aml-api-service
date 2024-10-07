92cfcc0f3b25a6d929c2e47c3f2643ea979cd9a9
karan-devslane <karan.budhwani@devslane.com>
2024-10-07 12:15:31 +0530

    updated database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,18 +1,5 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env' }); // Required
-
 export = {
-  local: {
-    username: process.env.AML_SERVICE_DB_USER,
-    password: process.env.AML_SERVICE_DB_PASS,
-    database: process.env.AML_SERVICE_DB_NAME,
-    host: process.env.AML_SERVICE_DB_HOST,
-    port: process.env.AML_SERVICE_DB_PORT,
-    dialect: 'postgres',
-    migrationStorageTableName: 'sequelize_meta',
-  },
-  development: {
+  [process.env.AML_SERVICE_APPLICATION_ENV as string]: {
     username: process.env.AML_SERVICE_DB_USER,
     password: process.env.AML_SERVICE_DB_PASS,
     database: process.env.AML_SERVICE_DB_NAME,
